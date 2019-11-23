ca1c4e7701c192523cd43d98894021d949653c10
Excelnd <ihsanmpm@gmail.com>
2019-11-23 21:37:05 +0530

    Add Class UtilitiesTestParameterized

---
+++ src/com/ihs/UtilitiesTestParameterized.java
@@ -0,0 +1,44 @@
+package com.ihs;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class UtilitiesTestParameterized {
+
+    private Utilities util;
+
+    private String input;
+    private String output;
+
+    public UtilitiesTestParameterized(String input, String output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    @org.junit.Before
+    public void setup() {
+        util = new Utilities();
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object> testConditions() {
+        return Arrays.asList(new Object[][] {
+
+            {"ABCDEFFFF", "ABCDEF"},
+            {"AB88EFFFFFG", "AB8EFG"},
+            {"112233344556666", "123456"},
+            {"A", "A"}
+        });
+    }
+
+    @org.junit.Test
+    public void removePairs() throws Exception {
+        assertEquals(output, util.removePairs(input));
+    }
+}
